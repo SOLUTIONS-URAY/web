87d517ee0fe46cb30e0a11b62e658fe26c50cf16
MIMBOL228 <aekemerov@yandex.ru>
2024-11-29 16:09:06 +0500

    style: Add styling for filter buttons

--- frontend/src/components/FilterButton/FilterButton.tsx
+++ frontend/src/components/FilterButton/FilterButton.tsx
@@ -1,5 +1,6 @@
 import React, {Dispatch, FC, SetStateAction} from "react";
 import "./FilterButton.scss";
+import filter_arrow from "@/src/assets/down_arrow.svg";
 
 type KeyValue = {
     [key: string]: string;
@@ -21,7 +22,7 @@ export const FilterButton: FC<FilterButtonProps> = (props) => {
     const onChangeValue = ({target}: React.ChangeEvent<HTMLInputElement>) => {
         console.log(target.value, target.checked)
         props.onChangeSelectedValue((prevState) => {
-            if(target.checked)
+            if (target.checked)
                 return [...prevState, target.value];
             else
                 return prevState
@@ -33,14 +34,18 @@ export const FilterButton: FC<FilterButtonProps> = (props) => {
         props.onChangeVisible(prevState => !prevState);
     }
 
+    const isButtonActive = props.selectedValues.length > 0 && props.selectedValues.length !== Object.keys(props.possibleValues).length
+
     return (
-        <>
-            <div className={"filter_button_container filter_button_container_"+(props.isVisible && "visible")}>
+        <div className="filter_group">
+            <div
+                className={"filter_button_container filter_button_container_" + (isButtonActive && "active")}>
                 <button className="filter_button" onClick={onChangeVisible}>
-                    {props.naming}
+                    <p className="filter_button_name">{props.naming}</p>
+                    <img src={filter_arrow} alt="" className="filter_button_icon"/>
                 </button>
             </div>
-            <div className={"filter_button_menu filter_button_menu_"+(props.isVisible && "visible")}>
+            <div className={"filter_button_menu filter_button_menu_" + (props.isVisible && "visible")}>
                 <div className="filter_possible_values_container">
                     {possibleValuesKeys.map((key) => {
                         const value = props.possibleValues[key];
@@ -48,23 +53,23 @@ export const FilterButton: FC<FilterButtonProps> = (props) => {
                             <div className="filter_possible_value" key={key}>
                                 <input
                                     type="checkbox"
-                                    id={"filter_"+props.en_name+"_"+value}
-                                    name={"filter_"+props.en_name}
+                                    id={"filter_" + props.en_name + "_" + value}
+                                    name={"filter_" + props.en_name}
                                     checked={props.selectedValues.includes(key)}
                                     value={key}
                                     onChange={onChangeValue}
                                 />
-                                <label htmlFor={"filter_"+props.en_name+"_"+value}>{value}</label>
+                                <label htmlFor={"filter_" + props.en_name + "_" + value}>{value}</label>
                             </div>
                         )
                     })}
                 </div>
                 <div className="filter_button_menu_save_button">
                     <button className="filter_button_menu_save_button" onClick={onChangeVisible}>
-                        Сохранить
+                        Подтвердить
                     </button>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
--- frontend/src/components/FilterButton/FilterMenu/FilterMenu.tsx
+++
@@ -1,5 +0,0 @@
-export const FilterMenu = () => {
-    return (
-        <></>
-    );
-};
